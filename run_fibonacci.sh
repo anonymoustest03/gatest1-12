6d5501e9cf08219577f8910af4f515f08978ff47
anonymoustest03 <sufssl101@outlook.com>
2025-10-29 18:37:07 +0000

    Create run_fibonacci.sh

---
+++ run_fibonacci.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# A simple shell function to calculate the Nth Fibonacci number
+# $1: The (N)th number to calculate
+
+fib() {
+  N=$1
+  a=0
+  b=1
+  
+  if [ $N -eq 0 ]; then
+    echo $a
+    return
+  fi
+
+  for ((i=2; i<=N; i++)); do
+    temp=$((a + b))
+    a=$b
+    b=$temp
+  done
+  
+  echo $b
+}
+
+# --- Main script execution ---
+
+# Set which Fibonacci number you want to find
+NUMBER_TO_CALCULATE=25
+
+echo "Calculating the Fibonacci number for: $NUMBER_TO_CALCULATE"
+
+# Call the function and store the result
+RESULT=$(fib $NUMBER_TO_CALCULATE)
+
+echo "-------------------------------------"
+echo "Result: $RESULT"
+echo "-------------------------------------"
